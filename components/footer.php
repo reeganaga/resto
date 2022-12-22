511c19b19d91bea4729a4da09ad08b8dec3034e4
reeganaga <rega.blank@gmail.com>
2022-12-22 08:57:21 +0700

    init

---
+++ components/footer.php
@@ -0,0 +1,39 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:theshishahouseseminyak@gmail.com">theshishahouseseminyak@gmail.com</a>
+         <a href="mailto:theshishahouseseminyak">theshishahouseseminyak@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="images/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>08:00am to 23:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">JL Camplung tanduk No 101 seseminyak,BALI - 400104</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:+62 81246105025">+62 81246105025</a>
+         <a href="tel:+62 81246105025">+62 81246105025</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; x <?= date('Y'); ?> by <span>XXX</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="images/loader.gif" alt="">
+</div>
